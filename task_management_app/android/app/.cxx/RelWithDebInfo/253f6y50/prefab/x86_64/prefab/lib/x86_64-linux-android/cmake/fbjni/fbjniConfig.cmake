fc871d5adcbf752b799e0b7f42e5a7c0bb414b71
Muskanbatra <mbatra1500@gmail.com>
2026-06-02 18:07:52 +0530

    commit first

---
+++ task_management_app/android/app/.cxx/RelWithDebInfo/253f6y50/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/muskanbatra/.gradle/caches/9.3.1/transforms/167bd46d6f9b4688eeeb844dac4ca47e/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/muskanbatra/.gradle/caches/9.3.1/transforms/167bd46d6f9b4688eeeb844dac4ca47e/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
